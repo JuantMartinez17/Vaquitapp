e9697ced4f21b3687cdf1d2cbad5917240b694e9
JuantMartinez17 <juantomasm0@gmail.com>
2026-03-26 00:12:44 -0300

    app basica funcionando, comenzando a definir estructura MVC

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const errorHandler = require('./middlewares/errorHandler');
-const routes = require('./routes');
+//const errorHandler = require('./middlewares/errorHandler');
+//const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', routes);
-
-app.use(errorHandler);
 
 module.exports = app;
