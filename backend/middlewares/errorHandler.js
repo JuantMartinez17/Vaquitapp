db77fe9fd654b346cd0f6960a64085d6b35cb045
JuantMartinez17 <juantomasm0@gmail.com>
2026-03-27 23:48:24 -0300

    Error code 23505

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,3 +1,11 @@
 //Middleware to handle basic postgreSql errors
 
-module.exports = () => {}
+module.exports = (err, req, res, next) => {
+    console.error("Error: ", err);
+
+    if(err.code === '23505') { // Unique violation error code
+        return res.status(400).json({ error: 'Duplicate entry. The value already exists.' });
+    }
+    
+    res.status(500).json({error: 'Internal Server Error' });
+}
