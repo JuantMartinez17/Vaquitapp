2ac9de28a81da08cfb8fcaa16eea59abe7466298
JuantMartinez17 <juantomasm0@gmail.com>
2026-05-19 17:10:03 -0300

    users basic routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,1 +1,7 @@
 const router = require('express').Router();
+const userController = require('../controllers/usersController')
+const auth = require('../middlewares/auth')
+
+router.post('/signup', userController.signup)
+router.post('/login', userController.login)
+router.get('/me', userController.getMe)
