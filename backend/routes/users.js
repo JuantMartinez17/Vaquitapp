f37f4528dcba67393776492ad0ab4f425082490c
JuantMartinez17 <juantomasm0@gmail.com>
2026-05-21 17:21:33 -0300

    re-starting backend dev :)

--- backend/routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const userController = require('../controllers/usersController')
-const auth = require('../middlewares/auth')
-
-router.post('/signup', userController.signup)
-router.post('/login', userController.login)
-router.get('/me', userController.getMe)
